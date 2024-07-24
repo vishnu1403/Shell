944c30d58086ecca2bd3f176ba278890fc5d9876
Vishnu Prasath M <136444824+vishnu1403@users.noreply.github.com>
2024-07-24 06:23:43 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+helper()
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -34,6 +37,13 @@ function list_users_with_read_access {
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
     fi
+}
+
+function helper{
+expected_cmd_args=2
+if [ $# -ne $expected_cmd_args]; then
+echo "Please execute the script with required arguments"
+echo "asd"
 }
 
 # Main script
